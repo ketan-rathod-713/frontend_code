0c1f2faa9b68686adec0d604b6434fe4c2c27840
Ketan Rathod <ketanrtd1@gmail.com>
2024-10-24 16:54:42 +0530

    javascript variables and datatypes

---
+++ javascript/1. fundamentals/01.code.js
@@ -0,0 +1,14 @@
+var accountId = 124
+console.log(accountId)
+
+const city = "surat"
+console.log(city)
+
+var email = "ketan@gmail.com"
+
+console.table([accountId, city, email])
+
+/* 
+Prefer not to use var
+because of block scope and global scope problem
+*/
---
+++ javascript/1. fundamentals/02.datatypes.js
@@ -0,0 +1,42 @@
+// pehle k javascript me classes aur vo sab nahi the
+
+// so the javascript me standard aa gaya he
+
+// "use strict" // if we write this at the top to treat JS code as a newer version
+
+// alert("hello") // for node.js it will give error Reference Error -> alert is not defined
+
+// There is different syntax is node to define alert
+
+// when to use semicolons and when not -> 
+
+console.log("something"); console.log(12)
+
+// code should be readable and future proof.
+
+// documentation ECMA -> it writes the standards which needs to followed by all the javascript.
+
+// This are the standards for ones who want to develop the browser or something like that.
+
+// There are many datatypes javascript provides like Date time etc. so we can checkout it from ecma documentation.
+
+let name= "hitesh"
+let age = 22
+let isLoggedIn = false
+
+// number
+// big int
+// boolean
+// string
+// null -> this is datatype and also a stand alone value
+// undefined -> when we haven't define the value yet.
+// null is a representation of the emtpy value. vo khali he. undefined nahi he.
+// symbol = uniquness
+// object
+
+console.log(typeof age)
+
+console.log(typeof null) // object // ye ek type nahi he but ek value he
+console.log(typeof undefined) // undefined // kyoki ye ek type he
+
+// checkout specification of javascript on ecma script documentation.
